603b002c9bf0678c74566d76b9abde19dbf61363
CIRES-Archive <jl.yu@uq.edu.au>
2024-06-17 10:16:05 +1000

    update callbacks.py

--- main.py
+++ main.py
@@ -13,17 +13,21 @@ def read_api_key(file_path='config.ini'):
     # Read the config.ini file
     config.read(file_path)
     # Access the api_key from the 'settings' section
-    try:
-        api_key = config['settings']['api_key']
-    except KeyError:
-        print("""Please configure your openai api_key in the file config.ini before runing the program.
+    error_msg = """Please configure your openai api_key in the file config.ini before running the program.
         
         The config.ini under the root directory of this project should have the following format:
         
         [settings]
         api_key = your_openai_api_key
         
-        """)
+        """
+    try:
+        api_key = config['settings']['api_key']
+    except KeyError:
+        print(error_msg)
+        sys.exit(1)
+    if api_key.strip() == '':
+        print(error_msg)
         sys.exit(1)
     return api_key
 
